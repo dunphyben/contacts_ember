0ab84752ac7ab518599793dbcf69ad59f2e1c2c0
Peter Tinti & Benjamin Paul Dunphy <student@epicodus.com>
2014-04-28 12:25:59 -0700

    Add nested routes to Edit page

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -4,6 +4,7 @@ Wikipages.Router.map(function() {
   this.resource('contacts', { path: '/' }, function() {
     this.resource('new_contact', { path: '/contacts/new' });
   });
-  this.resource('contact', { path: '/contacts/:id' });
-  this.resource('edit_contact', { path: '/contacts/:id/edit' });
+  this.resource('contact', { path: '/contacts/:id' }, function() {
+    this.resource('edit_contact', { path: '/contact/:id/edit' });
+  });
 });
